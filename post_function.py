8e5e46e3e5836df2965fcf3c7315eaa5c701b87a
OGaboriault <oliviergabo@gmail.com>
2024-12-18 15:37:37 -0500

    post_rel_density now uses argparse

--- post_function.py
+++ post_function.py
@@ -14,10 +14,10 @@ import matplotlib.pyplot as plt
 from lethe_pyvista_tools import *
 
 
-def starting_times(n, delta_n, plate_speed, first_starting_time, delta_starting_time, time_per_layer )
+def starting_times(n_layer, delta_n, plate_speed, first_starting_time, delta_starting_time, time_per_layer):
     """
     Input:
-        n : number of layer.
+        n_layer : number of layer.
         delta_n : Layer thikness.
         plate_speed : Measuring plate average displacement speed.
         first_starting_time : Time at which the first blade start to move. 
@@ -29,10 +29,34 @@ def starting_times(n, delta_n, plate_speed, first_starting_time, delta_starting_
     """
     
     plate_displacement_time = delta_n / plate_speed
-    every_starting_time = np.empty(n)
+    every_starting_time = np.empty(n_layer)
     every_starting_time[0] = first_starting_time
     
-    for j in range(1, n):
+    for j in range(1, n_layer):
         every_starting_time[j] = every_starting_time[j - 1] + time_per_layer * delta_starting_time
     
     return every_starting_time
+
+def measuring_vtu_list(n_layer, time_step_list, measuring_time):
+    """
+    Input:
+        n : number of layer.
+        time_step_list : List of all the vtu time.
+        measuring_time : Measuring time array.
+
+    Output:
+        Numpy array of the vtu index we want to analyze. 
+    """
+    vtu_measure = np.array([], dtype=int)
+    
+    # Index
+    which_layer = 0
+
+    for j, t in enumerate(time_step_list):
+        if t >= measuring_time[which_layer]:
+            vtu_measure = np.append(vtu_measure, int(j))
+            which_layer += 1
+            if which_layer == n_layer:
+                break
+            
+    return vtu_measure
--- post_rel_density.py
+++ post_rel_density.py
@@ -7,170 +7,129 @@ Date: January 13th, 2024
 """
 #############################################################################
 '''Importing Libraries'''
-import sys
-
+import argparse
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from lethe_pyvista_tools import *
+from post_function import *
 
 #############################################################################
 
-# Take case path as argument
-prm_file_names = np.array(
-    [
-        "25_03_200/25_03_200.prm",  # , Narval
-        "50_03_200/50_03_200.prm",  # , Narval
-    ])
-feeder_bool = False
+parser = argparse.ArgumentParser(description='Arguments for calculation relative density over the build plate')
+parser.add_argument("-of", "--output_folder", type=str, help="Output folder path", required=False, default = "./00_binary/" )
+parser.add_argument("-i", "--input", type=str, help="Parameter file", required=True)
+args, leftovers = parser.parse_known_args()
 
-# Loop over a
-for i in range(len(prm_file_names)):
-    print(f"\n\n Starting postprocessing for simulation number {i + 1} / {len(prm_file_names)}.")
-    # Variables from the first section of the .prm of the .prm file
-    with open("./" + prm_file_names[i], 'r') as file:
-        lines = file.readlines()  # Read all lines into a list
+prm_path = args.input
+output_path   = args.output_folder
 
-    number_of_layers = int((lines[3]).split('=')[1]) + 2
-    blade_speed = float((lines[4]).split('=')[1])
-    delta_B_P = float((lines[5]).split('=')[1])
-    delta_o = float((lines[6]).split('=')[1])
-    delta_n = float((lines[7]).split('=')[1])
-    GAP = float((lines[9]).split('=')[1])
-    first_layer_extrusion = float((lines[10]).split('=')[1])
-    n_layer_extrusion = float((lines[11]).split('=')[1])
-    first_starting_time = float((lines[12]).split('=')[1])
-    delta_starting_time = float((lines[13]).split('=')[1])
-    # extra_time_layer_zero = float((lines[14]).split('=')[1])
-    blade_thickness = 0.004267766952966369
+print(f"\n\nStarting postprocessing \n\n")
+# Variables from the first section of the .prm of the .prm file
+with open("./" + prm_path, 'r') as file:
+    lines = file.readlines()  # Read all lines into a list
 
-    # Create the particle object
-    prm_file_name = (prm_file_names[i]).split('/')[1]
+number_of_layers = int((lines[3]).split('=')[1]) + 2
+blade_speed = float((lines[4]).split('=')[1])
+delta_B_P = float((lines[5]).split('=')[1])
+delta_o = float((lines[6]).split('=')[1])
+delta_n = float((lines[7]).split('=')[1])
+GAP = float((lines[9]).split('=')[1])
+first_layer_extrusion = float((lines[10]).split('=')[1])
+n_layer_extrusion = float((lines[11]).split('=')[1])
+first_starting_time = float((lines[12]).split('=')[1])
+delta_starting_time = float((lines[13]).split('=')[1])
+# extra_time_layer_zero = float((lines[14]).split('=')[1])
+blade_thickness = 0.004267766952966369
 
-    pvd_name = 'out.pvd'
-    ignore_data = ['type', 'volumetric contribution', 'velocity', 'torque', 'fem_torque', 'fem_force']
-    particle = lethe_pyvista_tools((prm_file_names[i]).split('/')[0], prm_file_name, pvd_name, ignore_data=ignore_data)
-    #############################################################################
+# Create the particle object oo
 
-    # Bluid plate domain
-    # This string could be use to isolate the start and the end of the build plate (not for now)
-    input_string = particle.prm_dict["initial translation"]
-    x_min_Feeder = float(input_string[0].split(',')[0])  # --> This is the X translation of solid object #1
-    x_max_Feeder = float(input_string[1].split(',')[0])  # --> This is the X translation of solid object #1
-    x_min_BP = float(input_string[2].split(',')[0])  # --> This is the X translation of solid object #2
-    x_max_BP = float(input_string[3].split(',')[0])  # --> This is the X translation of solid object #3
+path = prm_path.split('/')[0]
+prm_file_name = prm_path.split('/')[-1]
 
-    # Lenght of the domain
-    input_string = particle.prm_dict["grid arguments"].split(',')
-    domain_x_length = float(input_string[4].split(':')[1])  # This is the dealii triangulation in X
-    domain_z_length = float(input_string[6].split(':')[0])  # This is the dealii triangulation in Z
-    build_plate_length = x_max_BP - x_min_BP
-    build_plate_area = domain_z_length * build_plate_length
+pvd_name = 'out.pvd'
+ignore_data = ['type', 'volumetric contribution', 'velocity', 'torque', 'fem_torque', 'fem_force']
+particle = lethe_pyvista_tools(path, prm_file_name, pvd_name, ignore_data=ignore_data)
+#############################################################################
 
-    # How much time it takes for a blade to move throw all the domain
-    # This will help to find the starting time of each blade, thus the mesuring time we should use
-    blade_time_per_layer = domain_x_length / blade_speed
+# Bluid plate domain
+# This string could be use to isolate the start and the end of the build plate (not for now)
+input_string = particle.prm_dict["initial translation"]
+x_min_Feeder = float(input_string[0].split(',')[0])  # --> This is the X translation of solid object #1
+x_max_Feeder = float(input_string[1].split(',')[0])  # --> This is the X translation of solid object #1
+x_min_BP = float(input_string[2].split(',')[0])      # --> This is the X translation of solid object #2
+x_max_BP = float(input_string[3].split(',')[0])      # --> This is the X translation of solid object #3
 
-    # Blade starting time
+# Lenght of the domain
+input_string = particle.prm_dict["grid arguments"].split(',')
+domain_x_length = float(input_string[4].split(':')[1])  # This is the dealii triangulation in X
+domain_z_length = float(input_string[6].split(':')[0])  # This is the dealii triangulation in Z
+build_plate_length = x_max_BP - x_min_BP
+build_plate_area = domain_z_length * build_plate_length
 
-    # Times it takes for a blade to cross the domain in the X direction
-    time_per_layer = (domain_x_length + blade_thickness) / blade_speed
+# Times it takes for a blade to cross the domain in the X direction
+time_per_layer = (domain_x_length + blade_thickness) / blade_speed
 
-    # Finding the departure time of all the blades
-    plates_speed = 0.002
-    plate_displacement_time = delta_n / plates_speed
-    every_starting_time = np.empty(number_of_layers)
-    every_starting_time[0] = first_starting_time
-    for j in range(1, number_of_layers):
-        every_starting_time[j] = every_starting_time[j - 1] + time_per_layer * delta_starting_time
+# Finding the departure time of all the blades (Measuring plate)
+plates_speed = 0.002
+every_starting_time = starting_times(number_of_layers, delta_n, plates_speed,first_starting_time, delta_starting_time, time_per_layer)
 
-    # Time step to measure the relative density off the powder on the feeder
-    measuring_time_Feeder_rel_density = every_starting_time
+# Time step to measure the relative density off the powder on the build plate
+# This parameter is arbitrarily. If the measurements are taken when the blade
+# is still on the BP, this number should be bigger.
+trigger_parameter = 0.86
+measuring_time = every_starting_time + trigger_parameter * time_per_layer
 
-    # Time step to measure the relative density off the powder on the build plate
-    measuring_time_PB_rel_density = every_starting_time + 0.86 * blade_time_per_layer
+# Creating the list of vtu to analyze.
+time = particle.time_list
+vtu_measure = measuring_vtu_list(number_of_layers, time, measuring_time)
 
-    # Create lists of vtus we want to analyse
-    vtu_measure_Feeder_slice = 0
-    vtu_measure_Feeder_rel_density = np.array([], dtype=int)
-    vtu_measure_BP_rel_density = np.array([], dtype=int)
+rel_density_each_layer = np.zeros(len(vtu_measure)) # Array for the layer relative density 
+rel_density_cumulative = np.zeros(len(vtu_measure)) # Array for the layer cummulative density 
+volume_on_BP           = np.zeros(len(vtu_measure))
 
-    # Loop over the vtu in the list and add the one right after the blade
-    which_layer = 0
-    index_F_RD = 0
-    time = particle.time_list
+print(f" ####### Lists of analysed vtus :"
+      f"\n BP_rel_density     :{vtu_measure}"
+      f"\n #######")
 
-    for j in range(len(particle.list_vtu)):
-        if time[j] > measuring_time_Feeder_rel_density[0] + x_max_Feeder * 1.1 / blade_speed:
-            vtu_measure_Feeder_slice = j
-            break
+# Constant used for the volume calculation
+volume_cst = 1. / 6. * np.pi
 
-    for j in range(len(particle.list_vtu)):
-        if index_F_RD < number_of_layers and time[j] >= measuring_time_Feeder_rel_density[index_F_RD]:
-            vtu_measure_Feeder_rel_density = np.append(vtu_measure_Feeder_rel_density, int(j) - 1)
-            index_F_RD += 1
+for index, k in enumerate(vtu_measure):
+    # Load the position in df
+    df_0 = particle.get_df(k)
+    
+    # Create a panda date frame (df)
+    # Pyvista data frame are a bit weird... the conditions used in this code
+    # to isolate the particle located over the build plate are not working with
+    # them. Because of this, I'm transfering the required data into a panda df
+    df = pd.DataFrame(df_0.points, columns=['x', 'y', 'z'])
+    df['diameter'] = df_0['diameter']
+    df.drop(['y', 'z'], axis=1) # We don't care about the y and z direction for now 
 
-        if time[j] >= measuring_time_PB_rel_density[which_layer]:
-            vtu_measure_BP_rel_density = np.append(vtu_measure_BP_rel_density, int(j))
-            which_layer += 1
-            if which_layer == number_of_layers:
-                break
+    condition = (x_min_BP <= df['x']) & (df['x'] <= x_max_BP)
 
-    BP_rel_density_each_layer = np.array([])
-    BP_rel_density_cumulative = np.array([])
-    volume_on_BP = np.zeros(number_of_layers)
-    volume_on_feeder = np.zeros(number_of_layers)
-    feeder_rel_density = np.array([])
-    # Loop over this new list
-    print(f" ####### Lists of analysed vtus :"
-          f"\n BP_rel_density     :{vtu_measure_BP_rel_density}"
-          f"\n #######")
-
-    # Constant used for the volume calculation
-    volume_cst = 1. / 6. * np.pi
-
-    for index, k in enumerate(vtu_measure_BP_rel_density):
-
-        # Load the position in df
-        df_0 = particle.get_df(k)
-
-        # Create a panda df
-        df = pd.DataFrame(df_0.points, columns=['x', 'y', 'z'])
-        df['diameter'] = df_0['diameter']
-        df.drop(['y', 'z'], axis=1)
-        condition = (x_min_BP <= df['x']) & (df['x'] <= x_max_BP)
-
-        # Every particles in this data-frame is on the build-plate.
-        df_filtered = df[condition]
+    # Every particles in this data-frame are on the build-plate.
+    df_filtered = df[condition]
+    volume_on_BP[index] = np.sum(volume_cst * (df_filtered["diameter"]) ** 3) # Sum of all the individual volumes
+    
+    if index != 0: # We don't compute the relative density of the first layer. (Doing the same thing as the experiments)
+        # Total vertical displacement of the build plate
+        total_height = index * delta_n
+        available_volume = total_height * build_plate_area
+        this_layer_height = delta_n
+        rel_density_cumulative[index] =  (volume_on_BP[index] - volume_on_BP[0]) / available_volume
+        rel_density_each_layer[index] = (volume_on_BP[index] - volume_on_BP[index - 2]) / (this_layer_height * build_plate_area)
 
 
-        volume_on_BP[index] = np.sum(volume_cst * (df_filtered["diameter"]) ** 3)
+print(f" Powder volume on build plate      : \n{volume_on_BP} \n ######## ")
+print(f" Relative density of each layer    : \n{rel_density_each_layer} \n ######## ")
+print(f" Cummulative relative density      : \n{rel_density_cumulative} \n ########")
 
-        if index != 0:
-            # Total vertical displacement of the build plate
-            total_height = index * delta_n
-            available_volume = total_height * build_plate_area
-            this_layer_height = delta_n
+np.save(output_path+ prm_file_name.split(".")[0] + "_LRD", rel_density_each_layer)
+np.save(output_path + prm_file_name.split(".")[0] + "_CRD", rel_density_cumulative)
+np.save(output_path + prm_file_name.split(".")[0] + "_number_of_layers", number_of_layers - 1)
 
-            BP_rel_density_cumulative = np.append(BP_rel_density_cumulative,
-                                                  (volume_on_BP[index] - volume_on_BP[0]) / available_volume)
-            BP_rel_density_each_layer = np.append(BP_rel_density_each_layer,
-                                                  (volume_on_BP[index] - volume_on_BP[index - 1]) / (
-                                                          this_layer_height * build_plate_area))
-
-    print(f" Powder volume on build plate      : \n{volume_on_BP} \n ######## ")
-    print(f" Powder volume in the reservoir    : \n{volume_on_feeder} \n ######## ")
-    print(f" Relative density in the reservoir : \n{feeder_rel_density} \n ######## ")
-    print(f" Relative density of each layer    : \n{BP_rel_density_each_layer} \n ######## ")
-    print(f" Cummulative relative density      : \n{BP_rel_density_cumulative} \n ########")
-
-    np.save("./00_binary/" + prm_file_name.split(".")[0] + "_PowderVolumeFeed", volume_on_feeder)
-    np.save("./00_binary/" + prm_file_name.split(".")[0] + "_RelDensityFeed", feeder_rel_density)
-    np.save("./00_binary/" + prm_file_name.split(".")[0] + "_LRD", BP_rel_density_each_layer)
-    np.save("./00_binary/" + prm_file_name.split(".")[0] + "_CRD", BP_rel_density_cumulative)
-    np.save("./00_binary/" + prm_file_name.split(".")[0] + "_number_of_layers", number_of_layers - 1)
-
-    print("Binary files saved!")
-
+print("Binary files saved!")
 print("Job is done")
